25aa8300cb353f4495b78491d0c0af8a04a69326
Hakan <hakankargin_2003@hotmail.com>
2020-12-16 00:52:31 +0300

    Create map

--- src/main/java/com/hakan/mapapi/Main.java
+++ src/main/java/com/hakan/mapapi/Main.java
@@ -14,11 +14,11 @@ public class Main extends JavaPlugin {
     }
 
     public static void setInstance(Plugin plugin) {
-        instance = plugin;
         if (instance != null) {
             Bukkit.getLogger().warning("MapAPI already registered.");
             return;
         }
+        instance = plugin;
         new SetupNMS().setup();
     }
 
